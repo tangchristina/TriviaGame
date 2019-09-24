18594edcc567a614b5ade15aed12b5ab60b7063f
Christina Tang <christinatangerine@hotmail.com>
2019-09-23 23:33:19 -0700

    starting over again

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 //triviaQuestion array holds the trivia questions, possible choices, and the correct answer.
-/*var triviaQuestions = [
+var triviaQuestions = [
     {
         question: "What is the world's most popular non-alcoholic drink?",
         choices: [ "Water", "Tea", "Coffee", "Soda"],
@@ -52,6 +52,7 @@ function timesUp() {
 function countdown() {
     counter--;
     $("#time").html("Time left: " + counter);
+    $("start").hide();
     if (counter===0) {
         timesUp();
     }
@@ -68,97 +69,39 @@ function displayQuestion() {
 //accesses the first question from the triviaQuestion array
     var question = triviaQuestions[currentQuestion].question;
 
-//accesses the possible choices from the triviaQuestion array
-    var choices = triviaQuestions[currentQuestion].choices;
-
 //displays the timer
     $("#time").html("Time left: " + counter);
 //displays the question
-    $("#game").html("<h4>${question}</h4>${displayChoices(choices)}");
-
-
+    $("#game").html("<h4>" + question + "</h4>");
 
 }
+//access the possible choices from the trivia questions array
+function displayChoices() {
 
-function displayChoices(choices) {
-    let result = '';
 
-    for (let i = 0; i < choices.length; i++) {
-        result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
+
+    for (let i = 0; i < triviaQuestions.length; i++) {
+        var choiceOne = triviaQuestions[i].choices[0];
+        var choiceTwo = triviaQuestions[i].choices[1];
+        var choiceThree = triviaQuestions[i].choices[2];
+        var choiceFour = triviaQuestions[i].choices[3];
+
+        $("game").html("<h4>" + choiceOne + "</h4>");
+        $("game").html("<h4>" + choiceTwo + "</h4>");
+        $("game").html("<h4>" + choiceThree + "</h4>");
+        $("game").html("<h4>" + choiceFour + "</h4>");
+        console.log(choiceOne);
     }
 
-    return result;
+
+  
 }
 
 displayQuestion();
+displayChoices();
 
 //jquery click events
 
 $(document).on("click", ".choice", function() {
 console.log("yeeee");
 });
-*/
-
-$(document).ready(function(){
-    $("#start").on("click", initializeGame);
-
-})
-
-var trivia = {
-        score=0,
-        losses=0,
-        currentQuestion=0,
-        counter=30,
-        timer,
-        questions: {
-           q1: "What is the world's most popular non-alcoholic drink?",
-           q2: "What is the world's fastest car?",
-           q3: "What is the highest paying job?"
-        },
-        choices: {
-            c1: [ "Water", "Tea", "Coffee", "Soda"],
-            c2: ["Lamborghini Aventador", "Koeniggseg Agera", "Porsche 911", "Bugatti Chiron"],
-            c3: ["Software engineer", "Physician", "Chemical Engineer", "Attorney"]
-        },
-        correctAnswer: {
-            a1: "Coffee",
-            a2: "Bugatti Chiron",
-            a3: "Physician"
-        }
-
-}
-
-function initializeGame () {
-    //INITIAL VALUES
-    trivia.score=0;
-    trivia.losses=0;
-    currentQuestion=0;
-    trivia.counter=5;
-    trivia.timer=setInterval(countdown, 1000);
-}
-
-//function that stops the timer and goes to the next question
-function timesUp() {
-    clearInterval(trivia.timer);
-    losses++;
-    //alert("Time's up! You lost. Next Question.");
-    nextQuestion();
-}
-
-//function that starts the timer
-function countdown() {
-    trivia.counter--;
-    $("#time").html("Time left: " + trivia.counter);
-    if (trivia.counter===0) {
-        timesUp();
-    }
-}
-
-//displays the timer
-$("#time").html("Time left: " + trivia.counter);
-//displays the question
-$("#question").html();
-//  empty last results
-$('#results').html('');
-// remove start button
-$('#start').hide();
